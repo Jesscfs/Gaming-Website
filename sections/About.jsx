0f1217c23a31e7aceb8c7e887c8a89cc20d785d1
Jesscfs <60202392+Jesscfs@users.noreply.github.com>
2022-12-22 02:14:13 -0600

    description edited

--- sections/About.jsx
+++ sections/About.jsx
@@ -22,17 +22,17 @@ const About = () => (
         variants={fadeIn('up', 'tween', 0.2, 1)}
         className="mt-[8px] font-normal sm:text-[32px] text-[20px] text-center text-secondary-white"
       >
-        In 1972 the very first home video game console the <span className="font-extrabold text-white">Odyssey</span>{' '}
+        In 1972, the very first video game home console the---<span className="font-extrabold text-white">Odyssey</span>---
         was manufactured by <span className="font-extrabold text-white">Magnavox</span>{' '} 
-        which has inspired other companies to come forth to create the best consoles that we play to this day.
-        There are 9 console generations with manufactures being {' '}
+        It inspired other companies to the forefront to create better consoles, very much like the ones we still play to this day.
+        There are 9 console generations from different manufactures which includes {' '}
         <span className="font-extrabold text-white">Nintendo,</span>{' '}
         <span className="font-extrabold text-white">Microsoft,</span>{' '}
         <span className="font-extrabold text-white">
-          Sony
+          Sony.
         </span>{' '}
-        and other console manufacturers. With many consoles to play from this has created a contradiction between fans on what is the best gaming console.
-        Come join us in the debate on what is the best console that will fulfill a person's need; from a home video game console to play at home to a gaming handheld console
+        With many consoles to choose from , this has created a dilema among fans to decide which gaming console is the best.
+        Come join us in the debate of what is the best console that will fulfill a person's need; from video game home console to a handheld gaming console
         for those that want to play anywhere they want. 
       </motion.p>
 
